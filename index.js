8d74ed035aa08ced427825dda02de1e0318e0733
Jeremy Liu <unswmercury@gmail.com>
2023-06-07 16:08:51 +1000

    Add user schema

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.post("/register", (req, res) => {
-  res.json("test ok");
+  const { username, password } = req.body;
+  res.json({ requestData: { username, password } });
 });
 
 app.listen(8080);
+
+// mongodb+srv://jeremyzeyuliu:FotoPie666@cluster0.h92dy9e.mongodb.net/
