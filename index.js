65244968a7adf7669be2aa33be16e9fe970842a3
Jeremy Liu <unswmercury@gmail.com>
2023-06-08 15:15:22 +1000

    Add user login API endpoint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("./src/models/User");
 require("dotenv").config();
 
@@ -17,19 +18,49 @@ mongoose.connect(`${process.env.MONGODB_CONNECTION_STRING}`);
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
-  // Generate salt for bcrypt
+  // Encrypt the password using bcrypt
   const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
 
   // Create user in DB
   try {
     const userDocument = await User.create({
       username,
-      password: bcrypt.hashSync(password, salt),
+      password: hashedPassword,
     });
-    res.json(userDocument);
+    res.status(200).json("Registration successful");
   } catch (e) {
     res.status(400).json(e);
   }
 });
 
+// Login controller
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if user exists in DB
+    const userDocument = await User.findOne({ username });
+
+    // Check if password is correct
+    const passwordMatch = bcrypt.compareSync(password, userDocument.password);
+
+    // If password matches, sign the JWT token and send to client
+    passwordMatch
+      ? jwt.sign(
+          { username, id: userDocument._id },
+          process.env.JWT_SECRET_KEY,
+          {},
+          (error, token) => {
+            if (error) throw error;
+            res.json(token);
+          }
+        )
+      : res.status(400).json("Invalid username or password. Please try again.");
+  } catch (e) {
+    res.status(200).json("User does not exist. Please register first.");
+  }
+});
+
+// Server port
 app.listen(process.env.SERVER_PORT);
